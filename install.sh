f78e6b1990c5158cc4039fde69ae8594ec3e4768
rlavrinenko <68317397+rlavrinenko@users.noreply.github.com>
2020-07-17 10:00:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,17 +19,17 @@ nginxpostfixadmincfg=/etc/nginx/conf.d/admin.$DOMEN.conf
 echo "$IP $mailhost" >> /etc/hosts 
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum-config-manager --enable remi-php74
+wget -P /etc/yum.repos.d/ https://raw.githubusercontent.com/rlavrinenko/mail/master/nginx/nginx.repo
 yum update -y
-yum install mariadb-server mariadb python2-certbot-dns-cloudflare certbot yum-utils dovecot dovecot-mysql unzip exim exim-mysql jq php-fpm php-mysql php-imap php-mbstring php-common php-pdo php-xml -y
+yum install mariadb-server mariadb python2-certbot-dns-cloudflare nginx certbot yum-utils dovecot dovecot-mysql unzip exim exim-mysql jq php-fpm php-mysql php-imap php-mbstring php-common php-pdo php-xml -y
+yum-config-manager --enable remi-php74
 mkdir -p /etc/letsencrypt/
 mkdir /var/vmail
 mkdir /etc/exim/dkim
 mkdir /var/www/$LOGIN/tmp -p
 chown exim:exim -R /var/vmail/
 chown -R exim:exim /var/spool/exim/
-wget -P /etc/yum.repos.d/ https://raw.githubusercontent.com/rlavrinenko/mail/master/nginx/nginx.repo
-yum install nginx
+mv $eximcfg $eximcfg.old
 wget -P /etc/exim/ https://raw.githubusercontent.com/rlavrinenko/mail/master/exim/exim.conf
 wget -P /var/www/$LOGIN/ https://sourceforge.net/projects/postfixadmin/files/postfixadmin/postfixadmin-3.2/postfixadmin-3.2.4.tar.gz
 mkdir /var/www/$LOGIN/mail
@@ -43,6 +43,7 @@ openssl genrsa -out /etc/exim/dkim/$DOMEN.key 2048
 openssl rsa -in /etc/exim/dkim/$DOMEN.key -pubout > /etc/exim/dkim/$DOMEN.pub
 echo "dns_cloudflare_email =$cfmail" > /etc/letsencrypt/cloudflareapi.cfg
 echo "dns_cloudflare_api_key =$cftok" >>/etc/letsencrypt/cloudflareapi.cfg
+
 certbot certonly --cert-name $DOMEN --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/cloudflareapi.cfg --server https://acme-v02.api.letsencrypt.org/directory -d "*.$DOMEN" -d $DOMEN
 #Опередлить если есть   (Thanks Tras2 https://gist.github.com/Tras2 )
 zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
@@ -67,8 +68,8 @@ curl -X POST "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/" \
         --data "{\"type\":\"A\",\"name\":\"mail.$DOMEN\",\"content\":\"$IP\",\"ttl\":1,\"proxied\":false}" | jq
 
 chown $LOGIN:$LOGIN -R /var/www/$LOGIN/	
-sed -e s/mailhostname/$mailhost/ $eximcfg
-sed -e s/maildomen/$DOMEN/ $eximcfg
+sed -i 's/mailhostname/$mailhost/g' $eximcfg
+sed -i 's/maildomen/$DOMEN/g' $eximcfg
 SYMBOLS=""
 for symbol in {A..Z} {a..z} {0..9}; do SYMBOLS=$SYMBOLS$symbol; done
 PWD_LENGTH=16  
@@ -78,7 +79,7 @@ for i in `seq 1 $PWD_LENGTH`
 do
 MAILPASS=$PASSWORD${SYMBOLS:$(expr $RANDOM % ${#SYMBOLS}):1}
 done
-sed -e s/DBpass/$MAILPASS $eximcfg
+sed -i 's/DBpass/$MAILPASS/g' $eximcfg
 
 systemctl restart nginx
 systemctl enable nginx
